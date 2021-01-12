43d14a5d9280f3cbda00b47c4b40f6ea8a098a6b
Timothé Mauzet <timothe.mauzet@gmail.com>
2021-01-12 13:34:29 +0100

    Base site web

---
+++ SITE/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  	<head>
+	    <meta charset="utf-8" />
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui" />
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <meta http-equiv="content-language" content="fr" />
+	    <meta name="language" content="fr" />
+	    <meta name="copyright" content="" /> <!-- EDIT -->
+	    <meta name="author" content="" /> <!-- EDIT -->
+	    <title></title> <!-- EDIT -->
+	    <meta name="description" content=""> <!-- EDIT -->
+	    <meta property="og:title" content="" /> <!-- EDIT -->
+	    <meta property="og:site_name" content="" /> <!-- EDIT -->
+	    <meta property="og:url" content="" /> <!-- EDIT -->
+	    <meta property="og:language" content="fr" />
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	    <link rel="stylesheet" media="screen" href="CSS/style.css" /> <!-- EDIT -->
+  	</head>
+  		<body>
+
+  			<div class="container">
+  				<div class="row">
+  					<div class="col-12 col-sm-12 col-md-12">
+  						
+    				</div>
+  				</div>
+  			</div>
+
+  			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+			<!-- ********************************************************
+			 * Copyright 2021
+			 * Timothe Mauzet (timothe.mauzet[at]outlook.com)
+			 ********************************************************* -->
+  		</body>
+</html>
