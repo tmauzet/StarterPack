c72519651bb2d46a461ca2055a49ac2f0a1a7785
Timothé Mauzet <timothe.mauzet@gmail.com>
2021-01-12 13:27:30 +0100

    Start script sh

---
+++ BASH/StartScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#########################################################
+# <TITRE>
+# Created by Timothé Mauzet (timothe.mauzet@outlook.com)
+# Version : x.x
+# Last modified : xx/xx/xxxx
+# #########################################################
